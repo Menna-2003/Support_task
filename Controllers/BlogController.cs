5536d2ab4a0843cbf3c28990c7921a9e8351536e
Menna Mohamed <2022170446@cis.asu.edu.eg>
2024-02-10 22:49:22 +0200

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using task.Models;
+
+namespace task.Controllers {
+    public class BlogController : Controller {
+        private static List<Blog> _blogs = new List<Blog>();
+
+        public IActionResult Index () {
+            return View();
+        }
+        public IActionResult AddBlog() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddBlog (Blog blog) {
+            int id = 0;
+            if ( _blogs.Count == 0 ) {
+                id = 1;
+            } else {
+                id = _blogs.Max( x => x.Id+1 );
+            }
+            blog.Id = id++;
+            _blogs.Add(blog);
+            return RedirectToAction( "index" );
+        }
+        #region GetAll
+        public IActionResult GetAllData() {
+            return View( _blogs );
+        }
+        #endregion
+
+        #region DeleteProduct
+        public IActionResult Delete ( int id ) {
+            Blog blog = _blogs.FirstOrDefault(x => x.Id == id);
+            _blogs.Remove(blog);
+
+            return RedirectToAction("GetAllData");
+        }
+        #endregion
+
+
+    }
+}
---
+++ Models/Blog.cs
@@ -0,0 +1,17 @@
+﻿namespace task.Models {
+    public class Blog {
+        public int Id {
+        get; set; }
+        public string Name {
+        get; set; }
+        public string Description {
+        get; set; }
+        public float price {
+        get; set; }
+        public int Quantity { get; set; }
+        public bool EnableSize { get; set; }
+
+        public Company company { get; set; }
+
+    }
+}
